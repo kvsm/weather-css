7e2635d01e67b111e49e694a4ed4838941cd902d
Kevin Smith <kvsm81@gmail.com>
2016-12-06 21:38:26 +0000

    Added 7-1 structure to stylesheets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import "./styles/styles.scss"
+import './stylesheets/main.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
